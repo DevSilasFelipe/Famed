6fcf67ebef07c36e2e20fbe6e160b86230fb1fd2
DevSilasFelipe <silas.evangelista@aluno.cps.sp.gov.br>
2026-05-01 15:32:53 +0000

    add `index.html` and configs of `navbar`

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const hamburguer = document.getElementById('hamburguer');
+const meni = document.getElementById('menu');
+
+hamburguer.addEventListener('click', () => {
+    const isOpen = menu.classList.toggle('open');
+    hamburguer.classList.toggle('active', isOpen);
+    hamburguer.setAttribute('aria-expanded', isOpen);
+});
+
+//Fecha o menu ao clicar em um link
+document.querySelectorAll('nav-list a').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('open');
+        hamburguer.classList.remove('active');
+        hamburguer.setAttribute('arial-expanded', false);
+    });
+});
